e4008b50f2390fc6ca0f6edba0fe207e4a953edf
kijae <kimkijea7619@hanyang.ac.kr>
2023-09-12 20:04:25 +0900

    second commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Icewall!'
+    return 'Hello, Icewall'
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
